dad668e95e22e23d4255743de47c47622007b74f
adua <6817006@muls.edu.mn>
2020-10-13 13:30:55 +0800

    rosliin anhnii file uusgeh

---
+++ dadlaga.js
@@ -0,0 +1,6 @@
+var mashin = ['prius10', 'prius11','prius20','prius30']
+/* mashin[0] = [prius10]
+mashin[1] = [prius11]
+mashin[2] = [prius20]
+mashin[mashin length] = [prius30]*/
+document.getElementById("jishee").innerHTML = (mashin);
